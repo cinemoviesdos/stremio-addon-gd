d6aefa067277e3b9b014d811ce57d788fa0b4b76
cinemoviesdos <cinemoviesdos@gmail.com>
2025-08-26 20:12:29 -0400

    Delete sync/sync/stremio_update.py

--- sync/sync/stremio_update.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def update_stremio():
-    try:
-        subprocess.run(['stremio', '--update'], check=True)
-        print("Stremio actualizado correctamente.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error al actualizar Stremio: {e}")
